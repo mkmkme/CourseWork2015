eba79e9ca1fae974fe663ae7ec5636c9bd9cef95
Reverend Homer <mk.43.ecko@gmail.com>
2015-11-17 22:12:31 +0300

    Initial commit

---
+++ src/ru/reverendhomer/Main.java
@@ -0,0 +1,10 @@
+package ru.reverendhomer;
+
+/**
+ * Created by ecko on 17.11.15.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello nugop!");
+    }
+}
